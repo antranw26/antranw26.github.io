5db11075057a3b6e0763c7e8b188bc2af82691c4
antranw26 <tranthanhan26082005@gmail.com>
2025-04-01 10:43:51 +1100

    java study

---
+++ week4/script.js
@@ -0,0 +1,12 @@
+console.log("hi");
+let name = "an";
+//comment//
+console.log("hi,i am", name);
+let myID = 12345;
+console.log("hi my student id is", myID);
+name = "abcd";
+console.log("hi, i am", name);
+let Newname = prompt("what is your name");
+console.log("hello", Newname);
+let myCity = "Mel";
+console.log("hello", Newname, myCity);
