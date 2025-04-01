a6b1715e7bd37f6a2f7294d14dc13f23d0498eae
antranw26 <tranthanhan26082005@gmail.com>
2025-04-01 12:08:57 +1100

    upd

--- week4/script.js
+++ week4/script.js
@@ -2,12 +2,53 @@ console.log("hi");
 let name = "an";
 //comment//
 console.log("hi,i am", name);
+//number variables
+let num = 10
+let num2= parseInt("20");
+let sum= num + num2
+console.log("total is", sum)
+
+//+ is for addition, joining text
+//-b for subtraction
+//* fpr multiplication
+// / division
+//boolean variables - true or false
+let isThisEvening = false;
+let isThisRmit = true
+
+//object varibles {}
+const myStudentDetails = {
+    name: "an"
+    myID: 1234,
+    hometown: Melbourne
+};
+ const weather = "sunny"
+ if weather = "sunny"
+ {
+    console.log("today is sunny weather")
+ } else{
+    console.log("too bad no sun today")
+ }
+const msg = `<h1> today's weather is ${weather}<p>
+this is so nice to see the sun</p></h1>`;
 let myID = 12345;
 console.log("hi my student id is", myID);
 name = "abcd";
 console.log("hi, i am", name);
 let Newname = prompt("what is your name");
 console.log("hello", Newname);
+//declare a variable called myName to contain my name
 let myCity = "Mel";
 console.log("hello", Newname, myCity);
-// if you know the value is goin to change define it using const
+// if you know the value is not goin to change define it using const or else use let//
+
+let array = [2, 4,6,8,10]
+let student1 = Tom
+let studentArray = [Tom , monkey , ok]
+console.log(studentArray[2])
+console.log(studentArray[0])
+{
+    let a = 10
+    console.log("hello", a)
+}
+console.log("hello",a)
